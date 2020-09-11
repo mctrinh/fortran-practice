db9d5258de3b020c82926d6d1627ae0a6add3ccf
mctrinh <mctrinh@jbnu.ac.kr>
2020-09-11 23:48:08 +0900

    2nd commit

--- Console1/basic.f90
+++
@@ -1,8 +0,0 @@
-program fortrantut
-    implicit none           ! force you to declare all of your variables
-    character*20 :: name
-    print *, "What's your name "    ! not using ; to show it to the screen
-    read *, name
-    print *, "Hello ", name
-    
-end program fortrantut    
