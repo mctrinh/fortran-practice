1d06a7d07a3e7e09a9b2b243516b2b128b313a12
mctrinh <mctrinh@jbnu.ac.kr>
2020-09-12 22:50:55 +0900

    3rd regular commit

--- Console1/basic0.f90
+++ Console1/basic0.f90
@@ -1,9 +1,9 @@
-! program basic0
-!    implicit none           ! force you to declare all of your variables
-!    character*20 :: name
-!    print *, "What's your name "    ! not using ; to show it to the screen
-!    read *, name                    ! read input from keyboard, store in the string 'name'
-!    print *, "Hello ", name         ! print the Hello message, immediately close, can't see the message!
-#    pause                           ! use 'pause' to hold the result screen until hitting 'Enter'
+ program basic0
+    implicit none           ! force you to declare all of your variables
+    character*20 :: name
+    print *, "What's your name "    ! not using ; to show it to the screen
+    read *, name                    ! read input from keyboard, store in the string 'name'
+    print *, "Hello ", name         ! print the Hello message, immediately close, can't see the message!
+    pause                           ! use 'pause' to hold the result screen until hitting 'Enter'
     
-! end program basic0
+ end program basic0
