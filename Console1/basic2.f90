c8051e3de380db336e9fc4e2005e7bd57d7bc417
mctrinh <mctrinh@jbnu.ac.kr>
2020-09-13 10:56:09 +0900

    4th regular commit

---
+++ Console1/basic2.f90
@@ -0,0 +1,19 @@
+program basic2
+    implicit none
+    real, parameter :: PI = 3.1415           ! 'real' mean double, float. Variable must start with a letter, variables are case insensitive
+    real :: r_num1 = 0.0, r_num2 = 0.0
+    double precision :: dbl_num = 1.1111111111111111d+0
+    integer :: i_num1 = 0, i_num2 = 0
+    logical :: can_vote = .true.
+    character (len = 10) :: month
+    complex :: com_num = (2.0, 4.0)
+    print *, "Biggest Real ", huge(r_num1)
+    print *, "Biggest Int ", huge(i_num1)
+    print *, "Smallest Real", tiny(r_num1)
+    print "(a4, i1)", "Int ", kind(i_num1)      ! return the number of bytes of each variable type
+    print "(a5, i1)", "Real ", kind(r_num1)     ! (a5, i1) formated prints
+    print "(a7, i1)", "Double ", kind(dbl_num)
+    print "(a8, i1)", "Logical ", kind(can_vote)
+    pause
+    
+end program basic2
